a558fff66216f1c62262602d742c205e122ee864
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-12-11 17:00:53 +0100

    test rules

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -5,7 +5,7 @@ import { Tutorial } from '../models/tutorial.model';
 
 const baseUrl = 'http://backend-api.com/api/tutorials';
 
-console.log("test rulds");
+console.log("test ruls");
 
 @Injectable({
   providedIn: 'root',
